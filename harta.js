5c647903b4109606c0e09e504c3f944029d137cb
ioanaralucad <raludret@yahoo.com>
2022-03-16 11:54:01 +0200

    initial commit

---
+++ harta.js
@@ -0,0 +1,144 @@
+$(document).ready(function(){
+  $(".hide").hide();
+  
+    $(".hide").click(function(){
+    $(".hide").hide();
+  });
+
+  $("#sticker01").click(function(){
+    $(".hide").hide();
+    $(".sticker01-txt").fadeIn(300);
+  });
+
+    $("#sticker02").click(function(){
+       $(".hide").hide();
+    $(".sticker02-txt").fadeIn(300);
+  });
+
+    $("#sticker03").click(function(){
+       $(".hide").hide();
+    $(".sticker03-txt").fadeIn(300);
+  });
+
+  $("#sticker04").click(function(){
+    $(".hide").hide();
+ $(".sticker04-txt").fadeIn(300);
+});
+
+ $("#sticker05").click(function(){
+    $(".hide").hide();
+ $(".sticker05-txt").fadeIn(300);
+});
+   
+$("#sticker06").click(function(){
+  $(".hide").hide();
+$(".sticker06-txt").fadeIn(300);
+});
+
+$("#sticker07").click(function(){
+  $(".hide").hide();
+$(".sticker07-txt").fadeIn(300);
+});
+
+$("#sticker08").click(function(){
+  $(".hide").hide();
+$(".sticker08-txt").fadeIn(300);
+});
+
+$("#sticker09").click(function(){
+  $(".hide").hide();
+$(".sticker09-txt").fadeIn(300);
+});
+
+$("#sticker10").click(function(){
+  $(".hide").hide();
+$(".sticker10-txt").fadeIn(300);
+});
+
+$("#sticker11").click(function(){
+  $(".hide").hide();
+$(".sticker11-txt").fadeIn(300);
+});
+
+$("#sticker12").click(function(){
+  $(".hide").hide();
+$(".sticker12-txt").fadeIn(300);
+});
+
+$("#sticker13").click(function(){
+  $(".hide").hide();
+$(".sticker13-txt").fadeIn(300);
+});
+
+$("#sticker14").click(function(){
+  $(".hide").hide();
+$(".sticker14-txt").fadeIn(300);
+});
+
+$("#sticker15").click(function(){
+  $(".hide").hide();
+$(".sticker15-txt").fadeIn(300);
+});
+
+$("#sticker16").click(function(){
+  $(".hide").hide();
+$(".sticker16-txt").fadeIn(300);
+});
+
+$("#sticker17").click(function(){
+  $(".hide").hide();
+$(".sticker17-txt").fadeIn(300);
+});
+
+$("#sticker18").click(function(){
+  $(".hide").hide();
+$(".sticker18-txt").fadeIn(300);
+});
+
+$("#sticker19").click(function(){
+  $(".hide").hide();
+$(".sticker19-txt").fadeIn(300);
+});
+
+$("#sticker20").click(function(){
+  $(".hide").hide();
+$(".sticker20-txt").fadeIn(300);
+});
+
+$("#sticker21").click(function(){
+  $(".hide").hide();
+$(".sticker21-txt").fadeIn(300);
+});
+
+$("#sticker22").click(function(){
+  $(".hide").hide();
+$(".sticker22-txt").fadeIn(300);
+});
+
+$("#sticker23").click(function(){
+  $(".hide").hide();
+$(".sticker23-txt").fadeIn(300);
+});
+
+$("#sticker24").click(function(){
+  $(".hide").hide();
+$(".sticker24-txt").fadeIn(300);
+});
+
+$("#sticker25").click(function(){
+  $(".hide").hide();
+$(".sticker25-txt").fadeIn(300);
+});
+
+$("#sticker26").click(function(){
+  $(".hide").hide();
+$(".sticker26-txt").fadeIn(300);
+});
+
+$("#sticker27").click(function(){
+  $(".hide").hide();
+$(".sticker27-txt").fadeIn(300);
+});
+
+});
+
